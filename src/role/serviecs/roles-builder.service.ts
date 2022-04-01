73cbd066808a1f9379d55edd1cd1e712bd79f434
Abdulrahman Falyoun <falyoun.abdulrahman@gmail.com>
2022-04-01 09:58:14 +0300

    [Fix]: role builder

--- src/role/serviecs/roles-builder.service.ts
+++ src/role/serviecs/roles-builder.service.ts
@@ -1,67 +1,66 @@
 import { Injectable } from '@nestjs/common';
-import { RolesBuilder } from 'nest-access-control';
 import { AppRole } from '@app/role/enums/app-role.enum';
 import { JunoResources } from '@app/role/enums/grant-action.eum';
 
 @Injectable()
 export class RolesBuilderService {
-  private readonly rolesBuilder: RolesBuilder;
-  private createForSuperAdmin() {
-    this.rolesBuilder
-      .grant(AppRole.SUPER_ADMIN)
-      .extend(AppRole.ADMIN)
-      .createAny(JunoResources.ORGANIZATION)
-      .updateAny(JunoResources.ORGANIZATION)
-      .deleteAny(JunoResources.ORGANIZATION)
-      .readAny(JunoResources.ORGANIZATION);
-  }
-  private createForAdmin() {
-    this.rolesBuilder
-      .grant(AppRole.ADMIN)
-      .extend(AppRole.HR_MANAGER)
-      .createAny(JunoResources.ACCOUNT)
-      .deleteAny(JunoResources.ACCOUNT)
-      .updateAny(JunoResources.ACCOUNT)
-      .readAny(JunoResources.ACCOUNT);
-  }
-
-  private createForManager() {
-    this.rolesBuilder
-      .grant(AppRole.HR_MANAGER)
-      .extend(AppRole.MANAGER)
-      .createAny(JunoResources.INVOICE)
-      .readAny(JunoResources.INVOICE)
-      .updateAny(JunoResources.INVOICE)
-      .deleteAny(JunoResources.INVOICE);
-  }
-  private createForTeamMember() {
-    this.rolesBuilder
-      .grant(AppRole.MANAGER)
-      .extend(AppRole.USER)
-      .createOwn(JunoResources.INVOICE)
-      .readOwn(JunoResources.INVOICE)
-      .updateOwn(JunoResources.INVOICE)
-      .deleteOwn(JunoResources.INVOICE);
-  }
-  private createForUser() {
-    this.rolesBuilder
-      .grant(AppRole.USER)
-      .createAny(JunoResources.ORGANIZATION)
-      .readOwn(JunoResources.ORGANIZATION)
-      .createAny(JunoResources.ACCOUNT)
-      .readOwn(JunoResources.ACCOUNT)
-      .updateOwn(JunoResources.ACCOUNT)
-      .deleteOwn(JunoResources.ACCOUNT);
-  }
-  constructor() {
-    this.rolesBuilder = new RolesBuilder();
-    this.createForUser();
-    this.createForTeamMember();
-    this.createForManager();
-    this.createForAdmin();
-    this.createForSuperAdmin();
-  }
-  get RolesBuilder() {
-    return this.rolesBuilder;
-  }
+  // private readonly rolesBuilder: RolesBuilder;
+  // private createForSuperAdmin() {
+  //   this.rolesBuilder
+  //     .grant(AppRole.SUPER_ADMIN)
+  //     .extend(AppRole.ADMIN)
+  //     .createAny(JunoResources.ORGANIZATION)
+  //     .updateAny(JunoResources.ORGANIZATION)
+  //     .deleteAny(JunoResources.ORGANIZATION)
+  //     .readAny(JunoResources.ORGANIZATION);
+  // }
+  // private createForAdmin() {
+  //   this.rolesBuilder
+  //     .grant(AppRole.ADMIN)
+  //     .extend(AppRole.HR_MANAGER)
+  //     .createAny(JunoResources.ACCOUNT)
+  //     .deleteAny(JunoResources.ACCOUNT)
+  //     .updateAny(JunoResources.ACCOUNT)
+  //     .readAny(JunoResources.ACCOUNT);
+  // }
+  //
+  // private createForManager() {
+  //   this.rolesBuilder
+  //     .grant(AppRole.HR_MANAGER)
+  //     .extend(AppRole.MANAGER)
+  //     .createAny(JunoResources.INVOICE)
+  //     .readAny(JunoResources.INVOICE)
+  //     .updateAny(JunoResources.INVOICE)
+  //     .deleteAny(JunoResources.INVOICE);
+  // }
+  // private createForTeamMember() {
+  //   this.rolesBuilder
+  //     .grant(AppRole.MANAGER)
+  //     .extend(AppRole.USER)
+  //     .createOwn(JunoResources.INVOICE)
+  //     .readOwn(JunoResources.INVOICE)
+  //     .updateOwn(JunoResources.INVOICE)
+  //     .deleteOwn(JunoResources.INVOICE);
+  // }
+  // private createForUser() {
+  //   this.rolesBuilder
+  //     .grant(AppRole.USER)
+  //     .createAny(JunoResources.ORGANIZATION)
+  //     .readOwn(JunoResources.ORGANIZATION)
+  //     .createAny(JunoResources.ACCOUNT)
+  //     .readOwn(JunoResources.ACCOUNT)
+  //     .updateOwn(JunoResources.ACCOUNT)
+  //     .deleteOwn(JunoResources.ACCOUNT);
+  // }
+  // constructor() {
+  //   this.rolesBuilder = new RolesBuilder();
+  //   this.createForUser();
+  //   this.createForTeamMember();
+  //   this.createForManager();
+  //   this.createForAdmin();
+  //   this.createForSuperAdmin();
+  // }
+  // get RolesBuilder() {
+  //   return this.rolesBuilder;
+  // }
 }
